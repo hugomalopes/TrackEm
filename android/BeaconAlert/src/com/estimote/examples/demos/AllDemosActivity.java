947d4b20195e813f920852af97fa7a9b2c7c7466
Marcus Gomes <mvp.gomes1989@gmail.com>
2014-11-04 16:01:42 +0000

    Remove the unecessary activities.

--- android/BeaconAlert/src/com/estimote/examples/demos/AllDemosActivity.java
+++ android/BeaconAlert/src/com/estimote/examples/demos/AllDemosActivity.java
@@ -18,15 +18,7 @@ public class AllDemosActivity extends Activity {
 
     setContentView(R.layout.all_demos);
 
-    findViewById(R.id.distance_demo_button).setOnClickListener(new View.OnClickListener() {
-      @Override
-      public void onClick(View v) {
-        Intent intent = new Intent(AllDemosActivity.this, ListBeaconsActivity.class);
-        intent.putExtra(ListBeaconsActivity.EXTRAS_TARGET_ACTIVITY, DistanceBeaconActivity.class.getName());
-        startActivity(intent);
-      }
-    });
-    findViewById(R.id.notify_demo_button).setOnClickListener(new View.OnClickListener() {
+   findViewById(R.id.notify_demo_button).setOnClickListener(new View.OnClickListener() {
       @Override
       public void onClick(View v) {
         Intent intent = new Intent(AllDemosActivity.this, ListBeaconsActivity.class);
@@ -34,13 +26,6 @@ public class AllDemosActivity extends Activity {
         startActivity(intent);
       }
     });
-    findViewById(R.id.characteristics_demo_button).setOnClickListener(new View.OnClickListener() {
-      @Override
-      public void onClick(View v) {
-        Intent intent = new Intent(AllDemosActivity.this, ListBeaconsActivity.class);
-        intent.putExtra(ListBeaconsActivity.EXTRAS_TARGET_ACTIVITY, CharacteristicsDemoActivity.class.getName());
-        startActivity(intent);
-      }
-    });
+
   }
 }
