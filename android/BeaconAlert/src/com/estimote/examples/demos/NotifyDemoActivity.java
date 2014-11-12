88bebef00cb81f2c37f31f9e6b10f35fc21eb769
hugomalopes <huguinhoman@gmail.com>
2014-11-12 16:14:50 +0000

    Notification bug fixed

--- android/BeaconAlert/src/com/estimote/examples/demos/NotifyDemoActivity.java
+++ android/BeaconAlert/src/com/estimote/examples/demos/NotifyDemoActivity.java
@@ -31,93 +31,93 @@ import static com.estimote.sdk.BeaconManager.MonitoringListener;
  */
 public class NotifyDemoActivity extends Activity {
 
-  private static final String TAG = NotifyDemoActivity.class.getSimpleName();
-  private static final int NOTIFICATION_ID = 123;
+	private static final String TAG = NotifyDemoActivity.class.getSimpleName();
+	private static final int NOTIFICATION_ID = 123;
 
-  private BeaconManager beaconManager;
-  private NotificationManager notificationManager;
-  private Region region;
+	private BeaconManager beaconManager;
+	private NotificationManager notificationManager;
+	private Region region;
 
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
-    getActionBar().setDisplayHomeAsUpEnabled(true);
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.notify_demo);
+		getActionBar().setDisplayHomeAsUpEnabled(true);
 
-    Beacon beacon = getIntent().getParcelableExtra(ListBeaconsActivity.EXTRAS_BEACON);
-    region = new Region("regionId", beacon.getProximityUUID(), beacon.getMajor(), beacon.getMinor());
-    notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-    beaconManager = new BeaconManager(this);
+		Beacon beacon = getIntent().getParcelableExtra(ListBeaconsActivity.EXTRAS_BEACON);
+		region = new Region("regionId", beacon.getProximityUUID(), beacon.getMajor(), beacon.getMinor());
+		notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+		beaconManager = new BeaconManager(this);
 
-    // Default values are 5s of scanning and 25s of waiting time to save CPU cycles.
-    // In order for this demo to be more responsive and immediate we lower down those values.
-    beaconManager.setBackgroundScanPeriod(TimeUnit.SECONDS.toMillis(1), 0);
+		// Default values are 5s of scanning and 25s of waiting time to save CPU cycles.
+		// In order for this demo to be more responsive and immediate we lower down those values.
+		beaconManager.setBackgroundScanPeriod(TimeUnit.SECONDS.toMillis(1), 0);
 
-    beaconManager.setMonitoringListener(new MonitoringListener() {
-      @Override
-      public void onEnteredRegion(Region region, List<Beacon> beacons) {
-        postNotification("Hey you've got a promotion!");
-        setContentView(R.layout.notify_demo);
-      }
+		beaconManager.setMonitoringListener(new MonitoringListener() {
+			@Override
+			public void onEnteredRegion(Region region, List<Beacon> beacons) {
+				postNotification("Hey you've got a promotion!");
+				setContentView(R.layout.notify_demo);
+			}
 
-      @Override
-      public void onExitedRegion(Region region) {
-        postNotification("Comeback if you don't want lose this opportunity!");
-        setContentView(R.layout.activity_main);
-      }
-    });
-  }
+			@Override
+			public void onExitedRegion(Region region) {
+				postNotification("Comeback if you don't want lose this opportunity!");
+				setContentView(R.layout.activity_main);
+			}
+		});
+	}
 
-  @Override
-  public boolean onOptionsItemSelected(MenuItem item) {
-    if (item.getItemId() == android.R.id.home) {
-      finish();
-      return true;
-    }
-    return super.onOptionsItemSelected(item);
-  }
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		if (item.getItemId() == android.R.id.home) {
+			finish();
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
 
-  @Override
-  protected void onResume() {
-    super.onResume();
+	@Override
+	protected void onResume() {
+		super.onResume();
 
-    notificationManager.cancel(NOTIFICATION_ID);
-    beaconManager.connect(new BeaconManager.ServiceReadyCallback() {
-      @Override
-      public void onServiceReady() {
-        try {
-          beaconManager.startMonitoring(region);
-        } catch (RemoteException e) {
-          Log.d(TAG, "Error while starting monitoring");
-        }
-      }
-    });
-  }
+		notificationManager.cancel(NOTIFICATION_ID);
+		beaconManager.connect(new BeaconManager.ServiceReadyCallback() {
+			@Override
+			public void onServiceReady() {
+				try {
+					beaconManager.startMonitoring(region);
+				} catch (RemoteException e) {
+					Log.d(TAG, "Error while starting monitoring");
+				}
+			}
+		});
+	}
 
-  @Override
-  protected void onDestroy() {
-    notificationManager.cancel(NOTIFICATION_ID);
-    beaconManager.disconnect();
-    super.onDestroy();
-  }
+	@Override
+	protected void onDestroy() {
+		notificationManager.cancel(NOTIFICATION_ID);
+		beaconManager.disconnect();
+		super.onDestroy();
+	}
 
-  private void postNotification(String msg) {
-    Intent notifyIntent = new Intent(NotifyDemoActivity.this, NotifyDemoActivity.class);
-    notifyIntent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
-    PendingIntent pendingIntent = PendingIntent.getActivities(
-        NotifyDemoActivity.this,
-        0,
-        new Intent[]{notifyIntent},
-        PendingIntent.FLAG_UPDATE_CURRENT);
-        Notification notification = new Notification.Builder(NotifyDemoActivity.this)
-        .setSmallIcon(R.drawable.euro_notify)
-        .setContentTitle("You've got a promotion!")
-        .setContentText(msg)
-        .setAutoCancel(true)
-        .setContentIntent(pendingIntent)
-        .build();
-    notification.defaults |= Notification.DEFAULT_SOUND;
-    notification.defaults |= Notification.DEFAULT_LIGHTS;
-    notificationManager.notify(NOTIFICATION_ID, notification);
-  }
+	private void postNotification(String msg) {
+		Intent notifyIntent = new Intent(NotifyDemoActivity.this, NotifyDemoActivity.class);
+		notifyIntent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+		PendingIntent pendingIntent = PendingIntent.getActivities(
+				NotifyDemoActivity.this,
+				0,
+				new Intent[]{notifyIntent}, PendingIntent.FLAG_UPDATE_CURRENT |
+				PendingIntent.FLAG_ONE_SHOT);
+		Notification notification = new Notification.Builder(NotifyDemoActivity.this)
+		.setSmallIcon(R.drawable.euro_notify)
+		.setContentTitle("You've got a promotion!")
+		.setContentText(msg)
+		.setAutoCancel(true)
+		.setContentIntent(pendingIntent)
+		.build();
+		notification.defaults |= Notification.DEFAULT_SOUND;
+		notification.defaults |= Notification.DEFAULT_LIGHTS;
+		notificationManager.notify(NOTIFICATION_ID, notification);
+	}
 }
